855ae3b1bb377d63b65aa87043e7fe520ebd2cdd
Kasey Hinton <kaseyjameshinton@gmail.com>
2022-06-07 01:01:00 -0600

    convert to fast

--- src/components/server-watcher.js
+++ src/components/server-watcher.js
@@ -1,93 +1,195 @@
-import { LitElement, html, css, render } from "lit";
-import { badge } from "@vaadin/vaadin-lumo-styles/badge";
-import { Notification } from "@vaadin/notification";
 import { get } from "httpie";
+import { FASTElement, html, css, repeat } from "@microsoft/fast-element";
 
-import styles from "../styles/styles";
+const template = html`
+  <h1 spacing-bottom>Server Watcher</h1>
 
-import "@vaadin/grid";
-import "@vaadin/grid/vaadin-grid-sort-column.js";
-import "@vaadin/grid/vaadin-grid-column.js";
-import "@vaadin/horizontal-layout";
-import "@vaadin/vertical-layout";
-import "@vaadin/button";
-import "@vaadin/text-field";
-import "@vaadin/select";
-import "./my-dialog";
+  <horizontal-row between spacing-bottom>
+    <fast-button @click=${(x, c) => (x.dialogOpen = true)} appearance="accent"
+      >Connect server</fast-button
+    >
+    <fast-select
+      :value=${(x) => x.selectedPingInterval ?? "3600000"}
+      @change=${(x, c) => {
+        x.selectedPingInterval = c.event.target.value;
+        localStorage.setItem(
+          "server-watcher-selected-ping-interval",
+          x.selectedPingInterval
+        );
+        x.clearIntervals();
+        x.runIntervals();
+      }}
+    >
+      <fast-option value="60000">Every minute</fast-option>
+      <fast-option value="300000">Every 5 minutes</fast-option>
+      <fast-option value="600000">Every 10 minutes</fast-option>
+      <fast-option value="1800000">Every 30 minutes</fast-option>
+      <fast-option value="3600000">Every hour</fast-option>
+    </fast-select>
+  </horizontal-row>
+  <server-items>
+    ${repeat(
+      (x) => x.items,
+      html`
+        <server-item>
+          ${(x) => x.name} ${(x) => x.ipaddress} ${(x) => x.status}
+          <fast-button
+            @click=${(x, c) => {
+              c.parent.runServerWatch(x);
+            }}
+          >
+            Run
+          </fast-button>
+          <fast-button
+            @click=${(x, c) => {
+              c.parent.items = c.parent.items.filter(
+                (i) => i.ipaddress !== x.ipaddress
+              );
+              localStorage.setItem(
+                "server-watcher-list",
+                JSON.stringify(
+                  c.parent.items.filter((i) => i.ipaddress !== x.ipaddress)
+                )
+              );
+            }}
+          >
+            Delete
+          </fast-button>
+        </server-item>
+      `,
+      { positioning: true }
+    )}
+  </server-items>
+  <fast-dialog :hidden=${(x) => !x.dialogOpen}>
+    <div
+      style="width: 350px; padding: 24px; display: flex; flex-direction: column; gap: 24px; color: var(--neutral-foreground-rest);"
+    >
+      <h2>Server</h2>
+      <fast-text-field
+        style="width: 100%"
+        @input=${(x, c) => {
+          x.newServerName = c.event.target.value;
+        }}
+        :value=${(x) => x.newServerName}
+        placeholder="Name"
+      ></fast-text-field>
 
-class ServerWatcher extends LitElement {
-  static properties = {
-    _dialog: { type: Object, state: true },
-    _newserverName: { type: String, state: true },
-    _newserverIP: { type: String, state: true },
-    items: { type: Array, state: true },
-    _selectedPingInterval: { type: String, state: true },
-    _intervals: { type: Array, state: true },
-    pingIntervals: { type: Array, state: true },
-    notificationOpened: { type: Boolean, state: true },
+      <fast-text-field
+        @input=${(x, c) => {
+          x.newServerIP = c.event.target.value;
+        }}
+        :value=${(x) => x.newServerIP}
+        placeholder="IP Address"
+      ></fast-text-field>
+      <button-container>
+        <fast-button @click=${(x) => (x.dialogOpen = false)}>Close</fast-button>
+        <fast-button
+          @click=${(x, c) => {
+            x.items = [
+              ...x.items,
+              {
+                name: x._newServerName,
+                ipaddress: x._newServerIP,
+                status: "Pending",
+              },
+            ];
+
+            const item = x.items.find((i) => i.ipaddress === x._newServerIP);
+
+            x.runServerWatch(item);
+            x.newServerName = "";
+            x.newServerIP = "";
+            x.dialogOpen = false;
+          }}
+          appearance="accent"
+          >Add</fast-button
+        >
+      </button-container>
+    </div>
+  </fast-dialog>
+`;
+const styles = css`
+  :host {
+    display: flex;
+    flex-direction: column;
+    margin: 24px;
+  }
+
+  server-items {
+    display: flex;
+    flex-direction: column;
+  }
+
+  server-item {
+    display: flex;
+    align-items: center;
+    gap: 24px;
+    padding: 12px;
+    border-bottom: 1px solid #eee;
+  }
+
+  horizontal-row {
+    display: flex;
+    align-items: center;
+  }
+
+  [spacing-bottom] {
+    margin-bottom: 24px;
+  }
+
+  [between] {
+    justify-content: space-between;
+  }
+`;
+
+class ServerWatcher extends FASTElement {
+  static definition = {
+    name: "server-watcher",
+    template,
+    styles,
+    attributes: [
+      "newServerName",
+      "newServerIP",
+      "items",
+      "selectedPingInterval",
+      "intervals",
+      "dialogOpen",
+    ],
   };
 
   constructor() {
     super();
     const serverWatcherList = localStorage.getItem("server-watcher-list");
     this.items = serverWatcherList ? JSON.parse(serverWatcherList) : [];
-
-    this._selectedPingInterval =
-      localStorage.getItem("server-watcher-selected-ping-interval") ??
-      undefined;
-
-    this.pingIntervals = [
-      {
-        label: "Every minute",
-        value: "60000",
-      },
-      {
-        label: "Every 5 minutes",
-        value: "300000",
-      },
-      {
-        label: "Every 10 minutes",
-        value: "600000",
-      },
-      {
-        label: "Every 30 minutes",
-        value: "1800000",
-      },
-      {
-        label: "Every hour",
-        value: "3600000",
-      },
-    ];
+    this.selectedPingInterval =
+      localStorage.getItem("server-watcher-selected-ping-interval") ?? 3600000;
 
     this.items.forEach((item) => {
       this.runServerWatch(item);
     });
 
-    this._intervals = [];
+    this.intervals = [];
     this.runIntervals();
-  }
-
-  firstUpdated() {
-    this._dialog = this.shadowRoot?.querySelector("my-dialog");
   }
 
   runIntervals() {
     this.items.forEach((item) => {
       const interval = setInterval(() => {
         this.runServerWatch(item);
-      }, this._selectedPingInterval);
+      }, this.selectedPingInterval);
 
-      this._intervals.push(interval);
+      this.intervals.push(interval);
     });
   }
 
   clearIntervals() {
-    this._intervals.forEach((interval) => {
+    this.intervals.forEach((interval) => {
       clearInterval(interval);
     });
   }
 
   async runServerWatch(server) {
+    console.log(server);
     const result = await this.getDataFromIP(server.ipaddress);
     const idx = this.items.findIndex((i) => i.ipaddress === server.ipaddress);
     console.log(result);
@@ -107,232 +209,6 @@ class ServerWatcher extends LitElement {
       return "Error";
     }
   }
-
-  openNotification(message, theme, position) {
-    const notification = Notification.show(message, {
-      position,
-    });
-    notification.setAttribute("theme", theme);
-    this.notificationOpened = true;
-    const handleOpenChanged = (event) => {
-      if (!event.detail.value) {
-        this.notificationOpened = false;
-        notification.removeEventListener("opened-changed", handleOpenChanged);
-      }
-    };
-    notification.addEventListener("opened-changed", handleOpenChanged);
-  }
-
-  static styles = [
-    styles,
-    badge,
-    css`
-      vaadin-horizontal-layout[action-bar] {
-        align-items: flex-end;
-        justify-content: space-between;
-        margin-bottom: 24px;
-      }
-
-      main {
-        margin: 24px 0;
-      }
-    `,
-  ];
-  render() {
-    return html`
-      <h1>Server Watcher</h1>
-
-      <vaadin-horizontal-layout action-bar theme="between">
-        <vaadin-button
-          connect
-          theme="secondary"
-          @click="${() => this._dialog.open()}"
-          >Connect server</vaadin-button
-        >
-        <vaadin-select
-          label="Ping Interval"
-          .items="${this.pingIntervals}"
-          @value-changed=${(event) => {
-            this._selectedPingInterval = event.target.value;
-            localStorage.setItem(
-              "server-watcher-selected-ping-interval",
-              event.target.value
-            );
-            this.clearIntervals();
-            this.runIntervals();
-          }}
-          .value="${this._selectedPingInterval ??
-          this.pingIntervals[this.pingIntervals.length - 1].value}"
-        ></vaadin-select>
-      </vaadin-horizontal-layout>
-
-      <vaadin-grid .items="${this.items}">
-        <vaadin-grid-sort-column
-          header="Name"
-          path="name"
-          auto-width
-        ></vaadin-grid-sort-column>
-        <vaadin-grid-sort-column
-          header="IP Address"
-          path="ipaddress"
-          auto-width
-        ></vaadin-grid-sort-column>
-        <vaadin-grid-column
-          header="Status"
-          .renderer="${this.statusRenderer}"
-          auto-width
-        ></vaadin-grid-column>
-        <vaadin-grid-column
-          header="Actions"
-          .renderer="${this.actionsRenderer}"
-          auto-width
-        ></vaadin-grid-column>
-      </vaadin-grid>
-
-      <my-dialog
-        header="server"
-        .body=${html`
-          <vaadin-vertical-layout
-            style=" align-items: stretch;width: 300px;max-width: 100%;"
-          >
-            <style>
-              h1 {
-                margin: 0;
-              }
-              main {
-                margin: 0 0 24px 0;
-                display: flex;
-                flex-direction: column;
-              }
-            </style>
-            <h1>server</h1>
-            <main>
-              <vaadin-text-field
-                .value=${this._newserverName}
-                @input=${(event) => {
-                  this._newserverName = event.target.value;
-                }}
-                label="Name"
-              ></vaadin-text-field>
-              <vaadin-text-field
-                .value=${this._newserverName}
-                @input=${(event) => {
-                  this._newserverIP = event.target.value;
-                }}
-                label="IP Address"
-              ></vaadin-text-field>
-            </main>
-
-            <vaadin-horizontal-layout
-              style="justify-content: end;"
-              theme="spacing"
-            >
-              <vaadin-button @click="${() => this._dialog.close()}">
-                Cancel
-              </vaadin-button>
-              <vaadin-button
-                theme="primary"
-                @click="${() => {
-                  this.items = [
-                    ...this.items,
-                    {
-                      name: this._newserverName,
-                      ipaddress: this._newserverIP,
-                      status: "Pending",
-                    },
-                  ];
-
-                  const item = this.items.find(
-                    (i) => i.ipaddress === this._newserverIP
-                  );
-                  this.runServerWatch(item);
-
-                  this._newserverName = "";
-                  this._newserverIP = "";
-                  this._dialog.close();
-                }}"
-              >
-                Add
-              </vaadin-button>
-            </vaadin-horizontal-layout>
-          </vaadin-vertical-layout>
-        `}
-        @opened-changed=${(e) => {
-          console.log("open changed", e);
-        }}
-      >
-      </my-dialog>
-    `;
-  }
-
-  actionsRenderer = (root, _, model) => {
-    const server = model.item;
-    render(
-      html`
-        <vaadin-horizontal-layout theme="spacing">
-          <vaadin-button
-            @click=${async () => {
-              this.openNotification(
-                `Running ${server.ipaddress}`,
-                "primary",
-                "top-end"
-              );
-              this.runServerWatch(server);
-            }}
-            title="Run"
-            theme="icon"
-            aria-label="Close"
-          >
-            <vaadin-icon icon="vaadin:refresh"></vaadin-icon>
-          </vaadin-button>
-          <vaadin-button
-            @click=${() => {
-              //TODO: open modal with textarea to take script to run
-              // If no script present fallback to ping
-            }}
-            title="Script"
-            theme="icon"
-            aria-label="Script"
-          >
-            <vaadin-icon icon="vaadin:file-code"></vaadin-icon>
-          </vaadin-button>
-          <vaadin-button
-            @click=${() => {
-              this.items = this.items.filter(
-                (i) => i.ipaddress !== server.ipaddress
-              );
-              localStorage.setItem(
-                "server-watcher-list",
-                JSON.stringify(this.items)
-              );
-            }}
-            title="Delete"
-            theme="icon error"
-            aria-label="Close"
-          >
-            <vaadin-icon icon="vaadin:trash"></vaadin-icon>
-          </vaadin-button>
-        </vaadin-horizontal-layout>
-      `,
-      root
-    );
-  };
-
-  statusRenderer = (root, _, model) => {
-    const server = model.item;
-    render(
-      html`
-        <span
-          theme="badge ${server.status === "Online" ||
-          server.status === "Pending"
-            ? "success"
-            : "error"}"
-          >${server.status}</span
-        >
-      `,
-      root
-    );
-  };
 }
 
-customElements.define("server-watcher", ServerWatcher);
+FASTElement.define(ServerWatcher);
--- src/my-app.js
+++ src/my-app.js
@@ -1,120 +1,188 @@
-import { html, LitElement, css, nothing, html } from "lit";
 import page from "page";
+import { parseColorHexRGB } from "@microsoft/fast-colors";
 
-import "@vaadin/app-layout";
-import "@vaadin/app-layout/vaadin-drawer-toggle";
-import "@vaadin/icon";
-import "@vaadin/icons";
-import "@vaadin/tabs";
-import "@vaadin/button";
-import "@vaadin/horizontal-layout";
+import {
+  allComponents,
+  provideFASTDesignSystem,
+} from "@microsoft/fast-components";
+import { provideDesignSystem } from "@divriots/starter-furious";
+provideFASTDesignSystem().register(allComponents);
+provideDesignSystem().register();
 
-//TODO: determine whether fetch or a nodejs call needed and handle accordingly
+import {
+  accentPalette as accentPaletteToken,
+  PaletteRGB,
+  SwatchRGB,
+} from "@microsoft/fast-components";
 
-class MyApp extends LitElement {
-  static properties = {
-    _page: { type: String, state: true, attribute: false },
-  };
+import { FASTElement, html, css } from "@microsoft/fast-element";
+import "./components/server-watcher";
 
-  constructor() {
-    super();
-    this._page = undefined;
+const template = html`
+  <fast-toolbar>
+    <h3>servertools.kaseyhinton.com</h3>
+  </fast-toolbar>
+  ${(x) =>
+    x.page === "server-watcher" ? html`<server-watcher></server-watcher>` : ""}
+`;
+const styles = css`
+  :host {
+    display: block;
+    margin-top: 49px;
   }
 
-  openSnackbar = (text, action = "OK", timeout = 5000) => {
-    const snackbar = this.shadowRoot.querySelector("mwc-snackbar");
-    if (!snackbar) {
-      return;
-    }
-    snackbar.labelText = text;
-    const button = snackbar.querySelector('mwc-button[slot="action"]');
-    button.label = action;
-    snackbar.timeoutMs = timeout;
-    snackbar.open = true;
+  fast-toolbar {
+    width: 100%;
+    padding: 12px;
+    top: 0;
+    position: fixed;
+  }
+
+  :not(:defined) {
+    visibility: hidden;
+  }
+`;
+
+export class MyApp extends FASTElement {
+  static definition = {
+    name: "my-app",
+    template,
+    styles,
+    attributes: ["page"],
   };
 
-  async firstUpdated() {
-    this.addEventListener("open-snackbar", (event) => {
-      this.openSnackbar(event?.text, event?.action, event?.timeout);
-    });
+  connectedCallback() {
+    super.connectedCallback();
+
+    const color = parseColorHexRGB("#335599");
+    const swatch = SwatchRGB.from(color);
+    const palette = PaletteRGB.create(swatch);
+    accentPaletteToken.setValueFor(this, palette);
 
     page("/", async () => {
       page.show("/server-watcher");
     });
 
     page("/server-watcher", (ctx) => {
-      this.#changePage("server-watcher", () =>
-        import("./components/server-watcher.js")
-      );
+      this.page = "server-watcher";
     });
 
     page("*", () => {
-      this.#changePage("error");
+      this.page = "error";
     });
 
     page.start();
   }
-
-  #changePage(mainPage, importFunction) {
-    new Promise(async (res) => {
-      this._page = mainPage;
-
-      try {
-        await importFunction?.();
-        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-      } catch (error) {
-        console.warn(error);
-      }
-      res();
-    });
-  }
-
-  static styles = [
-    css`
-      :host {
-        display: flex;
-        flex-direction: column;
-        font-family: system-ui;
-      }
-
-      vaadin-icon {
-        box-sizing: border-box;
-        margin-inline-end: var(--lumo-space-m);
-        padding: var(--lumo-space-xs);
-      }
-
-      [hidden] {
-        display: none !important;
-      }
-    `,
-  ];
-
-  render() {
-    return html`
-      <vaadin-app-layout primary-section="drawer">
-        <vaadin-drawer-toggle slot="navbar"></vaadin-drawer-toggle>
-        <h1 slot="navbar">Software Toolkit</h1>
-        <vaadin-tabs slot="drawer" orientation="vertical">
-          <vaadin-tab>
-            <a tabindex="-1">
-              <vaadin-icon icon="vaadin:list"></vaadin-icon>
-              <span>Server Watcher</span>
-            </a>
-          </vaadin-tab>
-        </vaadin-tabs>
-        <main>
-          ${this._page === "server-watcher"
-            ? html`<server-watcher></server-watcher>`
-            : nothing}
-        </main>
-      </vaadin-app-layout>
-
-      <mwc-snackbar labelText="">
-        <mwc-button slot="action"></mwc-button>
-        <mwc-icon-button icon="close" slot="dismiss"></mwc-icon-button>
-      </mwc-snackbar>
-    `;
-  }
 }
 
-customElements.define("my-app", MyApp);
+FASTElement.define(MyApp);
+
+// class MyApp extends LitElement {
+//   static properties = {
+//     _page: { type: String, state: true, attribute: false },
+//   };
+
+//   constructor() {
+//     super();
+//     this._page = undefined;
+//   }
+
+//   openSnackbar = (text, action = "OK", timeout = 5000) => {
+//     const snackbar = this.shadowRoot.querySelector("mwc-snackbar");
+//     if (!snackbar) {
+//       return;
+//     }
+//     snackbar.labelText = text;
+//     const button = snackbar.querySelector('mwc-button[slot="action"]');
+//     button.label = action;
+//     snackbar.timeoutMs = timeout;
+//     snackbar.open = true;
+//   };
+
+//   async firstUpdated() {
+//     this.addEventListener("open-snackbar", (event) => {
+//       this.openSnackbar(event?.text, event?.action, event?.timeout);
+//     });
+
+//     page("/", async () => {
+//       page.show("/server-watcher");
+//     });
+
+//     page("/server-watcher", (ctx) => {
+//       this.#changePage("server-watcher", () =>
+//         import("./components/server-watcher.js")
+//       );
+//     });
+
+//     page("*", () => {
+//       this.#changePage("error");
+//     });
+
+//     page.start();
+//   }
+
+//   #changePage(mainPage, importFunction) {
+//     new Promise(async (res) => {
+//       this._page = mainPage;
+
+//       try {
+//         await importFunction?.();
+//         window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+//       } catch (error) {
+//         console.warn(error);
+//       }
+//       res();
+//     });
+//   }
+
+//   static styles = [
+//     css`
+//       :host {
+//         display: flex;
+//         flex-direction: column;
+//         font-family: system-ui;
+//       }
+
+//       vaadin-icon {
+//         box-sizing: border-box;
+//         margin-inline-end: var(--lumo-space-m);
+//         padding: var(--lumo-space-xs);
+//       }
+
+//       [hidden] {
+//         display: none !important;
+//       }
+//     `,
+//   ];
+
+//   render() {
+//     return html`
+//       <fast-button>Hello world</fast-button>
+//       <!-- <vaadin-app-layout primary-section="drawer">
+
+//         <vaadin-drawer-toggle slot="navbar"></vaadin-drawer-toggle>
+//         <h1 slot="navbar">Software Toolkit</h1>
+//         <vaadin-tabs slot="drawer" orientation="vertical">
+//           <vaadin-tab>
+//             <a tabindex="-1">
+//               <vaadin-icon icon="vaadin:list"></vaadin-icon>
+//               <span>Server Watcher</span>
+//             </a>
+//           </vaadin-tab>
+//         </vaadin-tabs>
+//         <main>
+//           ${this._page === "server-watcher"
+//         ? html`<server-watcher></server-watcher>`
+//         : nothing}
+//         </main>
+//       </vaadin-app-layout>
+
+//       <mwc-snackbar labelText="">
+//         <mwc-button slot="action"></mwc-button>
+//         <mwc-icon-button icon="close" slot="dismiss"></mwc-icon-button>
+//       </mwc-snackbar> -->
+//     `;
+//   }
+// }
+
+// customElements.define("my-app", MyApp);
